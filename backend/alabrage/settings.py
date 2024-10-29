f5301d6fedc655274c0cf8ae4b762513772a13e9
musa <mosasatar88@gmail.com>
2024-10-29 21:51:28 +0300

    1

--- backend/alabrage/settings.py
+++ backend/alabrage/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p-f+627$7**2h$-v1dvc_%e4@%%-7jlb73fttve1d$#@o5tk6^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG=False
+DEBUG=True
 
-ALLOWED_HOSTS = ['15.185.205.92' , 'localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
